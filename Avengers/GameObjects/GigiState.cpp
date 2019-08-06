959c14aa815b6de5fa5d83d77962f119fc55a066
thinhphanngoc97 <15520846@gm.uit.edu.vn>
2019-08-06 22:54:29 +0700

    offset spawn gigi rocket

--- Avengers/GameObjects/GigiState.cpp
+++ Avengers/GameObjects/GigiState.cpp
@@ -67,7 +67,7 @@ void GigiState::state_flying()
 		this->shootTimeCount = 0;
 		int direction = gigi->IsLeft() ? 1 : 5;
 		float offsetX = gigi->IsLeft() ? -16 : 16;
-		float offsetY = -5;
+		float offsetY = -22;
 		SpawnProjectTile::GetInstance()->SpawnBullet(gigi->GetPositionX() + offsetX, gigi->GetPositionY() + offsetY,
 			direction, BulletType::GIGIROCKET);
 	}
